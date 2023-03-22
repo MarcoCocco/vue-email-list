58fe45fcceed881af17652e285d820a2e9fcebea
MarcoCocco <marcococco28@gmail.com>
2023-03-22 13:52:38 +0100

    feat: aggiunta la richiesta api per una singola mail

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            emailList: [],
+            singleEmail: null,
         }
+    },
+
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+            this.singleEmail = res.data.response;
+            this.emailList.push(this.singleEmail);
+        });
     },
 
 }).mount('#app')
